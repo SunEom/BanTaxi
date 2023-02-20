1e808df65f8de495eb25278c5745040bd79a393c
SunEom <dkxl0701@gmail.com>
2023-02-20 16:36:26 +0900

    애플 로그인 추가

--- BanTaxi/Presentation/Login/LoginViewController.swift
+++ BanTaxi/Presentation/Login/LoginViewController.swift
@@ -12,14 +12,20 @@ import FirebaseAuth
 import GoogleSignIn
 import FirebaseCore
 import SnapKit
+import AuthenticationServices
+import CryptoKit
 
 class LoginViewController: UIViewController {
+    
     let disposeBag = DisposeBag()
+    
+    var currentNonce: String? // property for apple login
     
     let titleLabel = UILabel()
     let colorView = UIView()
     let bottomView = UIView()
     let googleLoginButton = UIButton()
+    let appleLoginButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +43,12 @@ class LoginViewController: UIViewController {
                 self.googleLoginRequest()
             })
             .disposed(by: disposeBag)
+        
+        appleLoginButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.startSignInWithAppleFlow()
+            })
+            .disposed(by: disposeBag)
     }
     
     private func attribute() {
@@ -45,10 +57,10 @@ class LoginViewController: UIViewController {
         colorView.backgroundColor = UIColor(named: "MainColor")
         
         bottomView.backgroundColor = .white
-        bottomView.layer.shadowColor = UIColor.black.cgColor //색상
-        bottomView.layer.shadowOpacity = 0.5 //alpha값
-        bottomView.layer.shadowRadius = 30 //반경
-        bottomView.layer.shadowOffset = CGSize.zero //위치조정
+        bottomView.layer.shadowColor = UIColor.black.cgColor
+        bottomView.layer.shadowOpacity = 0.5
+        bottomView.layer.shadowRadius = 30
+        bottomView.layer.shadowOffset = CGSize.zero
         bottomView.layer.masksToBounds = false
         bottomView.layer.cornerRadius = 20
         
@@ -57,13 +69,25 @@ class LoginViewController: UIViewController {
         titleLabel.textColor = .white
         
         googleLoginButton.setTitle("구글로 로그인하기", for: .normal)
-        googleLoginButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .semibold)
-        googleLoginButton.setImage(UIImage(named: "google"), for: .normal)
+        googleLoginButton.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
+        googleLoginButton.setImage(UIImage(named: "Google"), for: .normal)
         googleLoginButton.setTitleColor(.black, for: .normal)
         googleLoginButton.layer.cornerRadius = 5
         googleLoginButton.layer.borderColor = UIColor.gray.cgColor
         googleLoginButton.layer.borderWidth = 0.5
-        googleLoginButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 30)
+        googleLoginButton.imageEdgeInsets = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 10)
+        googleLoginButton.imageView?.contentMode = .scaleAspectFit
+        
+        appleLoginButton.setTitle("애플로 로그인하기", for: .normal)
+        appleLoginButton.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
+        appleLoginButton.setImage(UIImage(named: "Apple"), for: .normal)
+        appleLoginButton.setTitleColor(.black, for: .normal)
+        appleLoginButton.layer.cornerRadius = 5
+        appleLoginButton.layer.borderColor = UIColor.gray.cgColor
+        appleLoginButton.layer.borderWidth = 0.5
+        appleLoginButton.imageEdgeInsets = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 10)
+        appleLoginButton.imageView?.contentMode = .scaleAspectFit
+        
         
     }
     
@@ -71,7 +95,10 @@ class LoginViewController: UIViewController {
         [colorView, bottomView].forEach { view.addSubview($0)}
         
         colorView.addSubview(titleLabel)
-        bottomView.addSubview(googleLoginButton)
+        [googleLoginButton, appleLoginButton].forEach {
+            bottomView.addSubview($0)
+        }
+        
         
         colorView.snp.makeConstraints {
             $0.top.equalToSuperview()
@@ -93,7 +120,14 @@ class LoginViewController: UIViewController {
         
         googleLoginButton.snp.makeConstraints {
             $0.top.equalToSuperview().offset(60)
-            $0.height.equalTo(55)
+            $0.height.equalTo(40)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+        
+        appleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(googleLoginButton.snp.bottom).offset(20)
+            $0.height.equalTo(40)
             $0.leading.equalToSuperview().offset(20)
             $0.trailing.equalToSuperview().offset(-20)
         }
@@ -102,6 +136,7 @@ class LoginViewController: UIViewController {
 }
 
 
+//MARK: - Google 로그인
 extension LoginViewController {
     private func googleLoginRequest() {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
@@ -146,4 +181,106 @@ extension LoginViewController {
             }
         }
     }
+}
+
+
+//MARK: - Apple 로그인
+
+extension LoginViewController{
+    @available(iOS 13, *)
+    func startSignInWithAppleFlow() {
+      let nonce = randomNonceString()
+      currentNonce = nonce
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+      request.nonce = sha256(nonce)
+
+      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.delegate = self
+      authorizationController.performRequests()
+    }
+
+    @available(iOS 13, *) // - 5️⃣
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+}
+
+@available(iOS 13.0, *)
+extension LoginViewController: ASAuthorizationControllerDelegate {
+
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+            print("Unable to fetch identity token")
+            return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+            return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            print("credential", credential)
+
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if (error != nil) {
+                  print(error?.localizedDescription)
+                  return
+                }
+              
+            }
+        }
+  }
+
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+    // Handle error.
+    print("Sign in with Apple errored: \(error)")
+  }
+
 }
